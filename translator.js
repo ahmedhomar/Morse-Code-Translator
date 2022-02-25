b88211232503b24fc8fba007ab37862683e37b85
Ahmed Omar <ahmedomar@hey.com>
2022-02-25 12:12:39 +0000

    Created the English to Morse translator function

--- translator.js
+++ translator.js
@@ -35,6 +35,21 @@ const morseCodeObject = {
   "": " ",
 };
 
-// export const translateEnglishToMorse = (englishWord) => {
-//   return num1*num2
-// }
+export const translateEnglishToMorse = (englishInput) => {
+  englishInput = englishInput.split("");
+  for (let i = 0; i < englishInput.length; i++) {
+    englishInput[i] = englishInput[i].toLowerCase().split("");
+    englishInput[i].forEach((letter, j) => {
+      englishInput[i][j] = morseCodeObject[letter];
+    });
+    englishInput[i] = englishInput[i].join(" ");
+  }
+  messageOutput.innerHTML = englishInput.join(" ");
+};
+/*
+1. The function translateEnglishToMorse() takes a string as an argument and splits it into an array of characters.
+2. For each character in the array, it converts it to lowercase and splits it into an array of letters.
+3. For each letter in the array, it looks up the letter in the morseCodeObject object and returns the morse code for that letter.
+4. The function then joins the array of morse code letters into a string and returns it.
+
+*/
