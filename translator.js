d7eb5439f03bc9873356f907c9a284ccf1760c66
Ahmed Omar <ahmedomar@hey.com>
2022-02-25 11:44:27 +0000

    created some tests for the translator function

--- translator.js
+++ translator.js
@@ -1,3 +1,40 @@
+const messageInput = document.getElementById("text-input");
+const messageOutput = document.getElementById("translated-message");
+let message;
+
+// I'm using an object to store English letters and Morse characters as key:value pairs.
+// eslint-disable-next-line no-unused-vars
+const morseCodeObject = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "_.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  " ": "",
+  "": " ",
+};
+
 // export const translateEnglishToMorse = (englishWord) => {
 //   return num1*num2
 // }
--- translator.spec.js
+++ translator.spec.js
@@ -1,33 +1,41 @@
-// import {expect,it,describe} from "@jest/globals";
-// import {multiply} from "./sums";
+/* eslint-disable no-unused-vars */
+import { expect, it, describe } from "@jest/globals";
+import { translateEnglishToMorse } from "./translator.js";
+// @ts-ignore
 
-// describe("Multiply function tests", () => {
-//     it("Multiplies 5 and 2 to equal 10", ()=> {
-//         const result = multiply(5,2);
-//         expect(result).toBe(10)
-//     });
-//     it("Multiplies 10 and 10 to equal 100", ()=> {
-//         const result = multiply(10,10);
-//         expect(result).toBe(100)
-//     });
+describe("translates words from english to morse", () => {
+// positive test case for a single letter
+  it("should translate "a" to ".-", () => {
+    const result = translateEnglishToMorse("a");
+    expect(result).toBe(".-");
 
-// })
+  it("should translate "car" to "-.-..-.-.", () => {
+    const result = translateEnglishToMorse("car");
+    expect(result).toBe("-.-..-.-.");
 
-// import { translateEnglishToMorse } from "./translator.js";
-// import { expect, it, describe } from "@jest/globals";
+// positive test case for a sentence
+// it("should translate "Hello, my name is John and I'm a doctor living in London" to ".... . .-.. .-.. --- --..-- / -- -.-- / -. .- -- . / .. ... / .--- --- .... -. / .- -. -.. / .. .----. -- / .- / -.. --- -.-. - --- .-. / .-.. .. ...- .. -. --. / .. -. / .-.. --- -. -.. --- -.", () => {
+//     const result = translateEnglishToMorse("Hello, my name is John and I'm a doctor living in London");
+//     expect(result).toBe(".... . .-.. .-.. --- --..-- / -- -.-- / -. .- -- . / .. ... / .--- --- .... -. / .- -. -.. / .. .----. -- / .- / -.. --- -.-. - --- .-. / .-.. .. ...- .. -. --. / .. -. / .-.. --- -. -.. --- -.");
 
-// describe("translate words from english to morse", () => {
-// positive test case for a single word
-//   it("walk should translate correctly to morse code", () => {
-//     const result = translateEnglishToMorse("walk");
-//     expect(result).toBe(".-- .- .-.. -.-");
-//   });
+//  Negative tests for edge cases 
 
-// positive tests for a single letter
+// qat = A shrub that grows in the Middle East and Africa
+it("should translate "qat" to "--.- .- -", () => {
+  const result = translateEnglishToMorse("qat");
+  expect(result).toBe("--.- .- -");
 
-// positive tests for multiple words
+});
 
-// negative tests for edge cases
+it("should not translate "bicycle" to "-.....-.-.", () => {
+  const result = translateEnglishToMorse("bicycle");
+  expect(result).not.toBe("-.....-.-.");
+  
+});
+
+
 // });
+
+
 
 // describe("translates morse code to english", () => {});
